0947ac9ce7651d7dcd9bb5d3445e5e56bd148b87
Grace Shih <vocalise@gmail.com>
2019-12-19 05:12:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str_time) {
+  let hour = parseInt(str_time.split(":"), 10);
+  let minutes = parseInt(str_time.split(":"), 10);
+  if (hour < 12) {
+   return "Good Morning";
+  } else if ((hour >= 12) && (hour <= 17)) {
+    return "Good Afternoon";
+  } else if (hour > 17) {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById("greeting").textContent = str;
+}
