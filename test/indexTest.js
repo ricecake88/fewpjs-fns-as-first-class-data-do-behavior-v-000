092597ece46e363162335c9f199a209508732ceb
Grace Shih <vocalise@gmail.com>
2019-12-19 05:23:08 +0000

    Automatically backed up by Learn

--- test/indexTest.js
+++ test/indexTest.js
@@ -16,7 +16,6 @@ describe("index.js", () => {
       let testContent = "TEST"
       displayMessage(testContent)
       let content = document.getElementById("greeting").innerText
-      console.log(content)
       expect(content).to.equal(testContent)
     })
   })
